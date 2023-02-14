462bc4de4a255975f74c33a72b81dcc29e3c7220
Lidia Ros <lidiarosmontes@gmail.com>
2023-02-14 19:34:57 +0100

    structure

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div>CARLA ROCH</div>
+  </header>
+
+  <nav></nav>
+
+  <section></section>
+  <section></section>
+  <section></section>
+
+  <footer></footer>
+</template>
+
+<style scoped></style>
